ea18e076f672685911741cf2278222c60648ee07
Andy-git985 <andy.yu617@gmail.com>
2022-06-23 22:10:33 -0400

    destructuring and quantity cssid added

--- main.js
+++ main.js
@@ -5,28 +5,28 @@ const shopItemsData = [
     id: 'cgbpkywbtx',
     name: 'Casual Shirt',
     price: 45,
-    desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+    desc: 'A casual oxford button dowwn suitable for any occasion.',
     img: 'images/img-1.jpg',
   },
   {
     id: 'wcmrjsupoc',
     name: 'Office Shirt',
     price: 100,
-    desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+    desc: 'Office shirt great with a flashy tie.',
     img: 'images/img-2.jpg',
   },
   {
     id: 'fdrydindhn',
     name: 'T Shirt',
     price: 25,
-    desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+    desc: 'Comfortable 100% cotton crewneck T-shirt.',
     img: 'images/img-3.jpg',
   },
   {
     id: 'eygiazhcay',
     name: 'Mens Suit',
     price: 300,
-    desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.',
+    desc: 'When you are looking to nail that promotion or interview, reach for this.',
     img: 'images/img-4.jpg',
   },
 ];
@@ -34,17 +34,18 @@ const shopItemsData = [
 const generateShop = () => {
   return (shop.innerHTML = shopItemsData
     .map((x) => {
+      const { id, name, price, desc, img } = x;
       return `
-    <div class="item">
-      <img width="223" src="${x.img}" alt="" />
+    <div id=product-id-${id} class="item">
+      <img width="223" src="${img}" alt="" />
       <div class="details">
-        <h3>${x.name}</h3>
-        <p>${x.desc}</p>
+        <h3>${name}</h3>
+        <p>${desc}</p>
         <div class="price-quantity">
-        <h2>$${x.price}</h2>
+        <h2>$${price}</h2>
         <div class="buttons">
           <i class="bi bi-dash-lg"></i>
-          <div class="quantity">0</div>
+          <div id=${id} class="quantity">0</div>
           <i class="bi bi-plus-lg"></i>
         </div>
       </div>
