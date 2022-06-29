a4fac43ac4af984f9da2383ff0735a74e44a46e5
Andy-git985 <andy.yu617@gmail.com>
2022-06-28 23:09:34 -0400

    css styling for cart.html

--- src/cart.js
+++ src/cart.js
@@ -21,8 +21,20 @@ const generateCartItems = () => {
       <div class="cart-item">
         <img width="100" src="${search.img}" alt="" />
         <div class="details">
-          <div class="title-price-x"></div>
-          <div class="cart-buttons"></div>
+          <div class="title-price-x">
+            <h4 class="title-price">
+              <p>${search.name}</p>
+              <p class="cart-item-price">$ ${search.price}</p>
+            </h4>
+            <i class="bi bi-x-lg"></i>
+          </div>
+
+          <div class="buttons">
+            <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+            <div id=${id} class="quantity">${item}</div>
+            <i onclick="increment(${id})"class="bi bi-plus-lg"></i>
+          </div>
+
           <h3></h3>
         </div>
       </div>
@@ -41,3 +53,41 @@ const generateCartItems = () => {
 };
 
 generateCartItems();
+
+const increment = (id) => {
+  const selectedItem = id;
+  const search = basket.find((x) => x.id === selectedItem.id);
+  if (search === undefined) {
+    basket.push({
+      id: selectedItem.id,
+      item: 1,
+    });
+  } else {
+    search.item += 1;
+  }
+  localStorage.setItem('data', JSON.stringify(basket));
+  update(selectedItem.id);
+};
+const decrement = (id) => {
+  const selectedItem = id;
+  const search = basket.find((x) => x.id === selectedItem.id);
+  // handles typeError search undefined error in console when you hit minus and nothing is in local storage
+  if (search === undefined) return;
+  else if (search.item === 0) return;
+  else {
+    search.item -= 1;
+  }
+
+  update(selectedItem.id);
+  // Deleted items with 0 quantity from local storage
+  basket = basket.filter((x) => x.item !== 0);
+  // Deletes items with 0 quantity on re-render
+  generateCartItems();
+  localStorage.setItem('data', JSON.stringify(basket));
+};
+
+const update = (id) => {
+  const search = basket.find((x) => x.id === id);
+  document.getElementById(id).innerHTML = search.item;
+  calculation();
+};
